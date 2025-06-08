debf78f3a3ee1952d018d16a55537272b3db233e
kijiwon <kiji1313@gmail.com>
2025-06-08 18:02:44 +0900

    야자수 Mesh

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,10 @@ const $result = document.getElementById("result");
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffe287);
 
+// 축
+const axes = new THREE.AxesHelper(10);
+scene.add(axes);
+
 const camera = new THREE.PerspectiveCamera(
   50, // 시야 각
   $result.clientWidth / $result.clientHeight, // window의 종횡비
@@ -13,7 +17,7 @@ const camera = new THREE.PerspectiveCamera(
   1000 // 최대거리
 );
 // 위치 설정
-camera.position.set(5, 5, 5);
+camera.position.set(10, 10, 5);
 // 바라볼 좌표값 설정 - camera는 z축에 평행한 방향을 비춤
 camera.lookAt(0, 0, 0);
 
@@ -34,11 +38,11 @@ const bodyMaterial = new THREE.MeshStandardMaterial({
 // 아래쪽
 const bottomGeometry = new THREE.DodecahedronGeometry(2, 1);
 const bottom = new THREE.Mesh(bottomGeometry, bodyMaterial);
-scene.add(bottom);
+// scene.add(bottom);
 // 위쪽
 const topGeometry = new THREE.TetrahedronGeometry(0.8, 3);
 const top = new THREE.Mesh(topGeometry, bodyMaterial);
-scene.add(top);
+// scene.add(top);
 // 두 mesh를 합치기(top mesh의 position 설정)
 top.position.y = 1.7;
 
@@ -51,15 +55,78 @@ const leafMaterial = new THREE.MeshStandardMaterial({
 // 나뭇잎 줄기
 const stemGeometry = new THREE.CylinderGeometry(0.1, 0.1, 0.4);
 const stem = new THREE.Mesh(stemGeometry, leafMaterial);
-scene.add(stem);
+// scene.add(stem);
 // 줄기 위치 설정
 stem.position.y = 2.5;
 
+// 나뭇잎
 const leafGeometry = new THREE.SphereGeometry(0.5, 32, 16, 0, Math.PI / 3);
 const leaf = new THREE.Mesh(leafGeometry, leafMaterial);
-scene.add(leaf);
+// scene.add(leaf);
 leaf.position.set(-0.5, 2.4, -0.1);
 leaf.rotation.z = Math.PI / -2;
+
+// 나무
+// 나무 줄기
+const trunkMaterial = new THREE.MeshStandardMaterial({
+  color: 0xa38049,
+});
+const trunkGeometry = new THREE.CylinderGeometry(0.8, 1, 1.5);
+const trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+scene.add(trunk1);
+
+const trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk2.position.set(0.1, 1.3, 0);
+trunk2.scale.set(0.9, 0.9, 0.9);
+trunk2.rotation.z = THREE.MathUtils.degToRad(-5);
+scene.add(trunk2);
+
+const trunk3 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk3.position.set(0.2, 2.5, 0);
+trunk3.scale.set(0.8, 0.8, 0.8);
+trunk3.rotation.z = THREE.MathUtils.degToRad(-5);
+scene.add(trunk3);
+
+const trunk4 = new THREE.Mesh(trunkGeometry, trunkMaterial);
+trunk4.position.set(0.3, 3.5, 0);
+trunk4.scale.set(0.7, 0.7, 0.7);
+trunk4.rotation.z = THREE.MathUtils.degToRad(-8);
+scene.add(trunk4);
+
+// 나뭇잎
+const treeLeafMaterial = new THREE.MeshStandardMaterial({
+  color: 0x84ad88,
+  side: THREE.DoubleSide,
+});
+const treeLeafGeometry = new THREE.SphereGeometry(
+  2,
+  32,
+  16,
+  Math.PI / 3, // 잎의 각도 = 60도
+  Math.PI / 3 // 잎의 길이 = 60도
+);
+const treeLeaf1 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
+treeLeaf1.rotation.x = Math.PI / -2;
+treeLeaf1.position.set(0, 3.2, 2);
+scene.add(treeLeaf1);
+
+const treeLeaf2 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
+treeLeaf2.rotation.x = Math.PI / -2;
+treeLeaf2.rotation.z = Math.PI / 2;
+treeLeaf2.position.set(2, 3.2, 0);
+scene.add(treeLeaf2);
+
+const treeLeaf3 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
+treeLeaf3.rotation.x = Math.PI / -2;
+treeLeaf3.rotation.z = Math.PI;
+treeLeaf3.position.set(0, 3.2, -2);
+scene.add(treeLeaf3);
+
+const treeLeaf4 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
+treeLeaf4.rotation.x = Math.PI / -2;
+treeLeaf4.rotation.z = Math.PI / -2;
+treeLeaf4.position.set(-2, 3.2, 0);
+scene.add(treeLeaf4);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -72,9 +139,6 @@ controls.maxDistance = 10;
 controls.enableDamping = true;
 
 function animate() {
-  // box 회전
-  //   box.rotation.y += 0.01;
-  // box가 회전할 때마다 렌더링해야함(변경된 속성값 렌더링)
   renderer.render(scene, camera);
   controls.update();
   requestAnimationFrame(animate);
