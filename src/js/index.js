a0a49103e301aa4f0ed35c132589e7c579f90173
kijiwon <kiji1313@gmail.com>
2025-06-08 18:18:31 +0900

    야자수 Mesh 그룹화

--- src/js/index.js
+++ src/js/index.js
@@ -11,13 +11,13 @@ const axes = new THREE.AxesHelper(10);
 scene.add(axes);
 
 const camera = new THREE.PerspectiveCamera(
-  50, // 시야 각
+  100, // 시야 각
   $result.clientWidth / $result.clientHeight, // window의 종횡비
   0.1, // 최소거리
   1000 // 최대거리
 );
 // 위치 설정
-camera.position.set(10, 10, 5);
+camera.position.set(0, 10, 15);
 // 바라볼 좌표값 설정 - camera는 z축에 평행한 방향을 비춤
 camera.lookAt(0, 0, 0);
 
@@ -67,33 +67,40 @@ leaf.position.set(-0.5, 2.4, -0.1);
 leaf.rotation.z = Math.PI / -2;
 
 // 나무
-// 나무 줄기
+const tree = new THREE.Group();
+// 그룹화 - 나무 몸통
+const trunk = new THREE.Group();
+
 const trunkMaterial = new THREE.MeshStandardMaterial({
   color: 0xa38049,
 });
 const trunkGeometry = new THREE.CylinderGeometry(0.8, 1, 1.5);
 const trunk1 = new THREE.Mesh(trunkGeometry, trunkMaterial);
-scene.add(trunk1);
+trunk.add(trunk1);
 
 const trunk2 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk2.position.set(0.1, 1.3, 0);
 trunk2.scale.set(0.9, 0.9, 0.9);
 trunk2.rotation.z = THREE.MathUtils.degToRad(-5);
-scene.add(trunk2);
+trunk.add(trunk2);
 
 const trunk3 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk3.position.set(0.2, 2.5, 0);
 trunk3.scale.set(0.8, 0.8, 0.8);
 trunk3.rotation.z = THREE.MathUtils.degToRad(-5);
-scene.add(trunk3);
+trunk.add(trunk3);
 
 const trunk4 = new THREE.Mesh(trunkGeometry, trunkMaterial);
 trunk4.position.set(0.3, 3.5, 0);
 trunk4.scale.set(0.7, 0.7, 0.7);
 trunk4.rotation.z = THREE.MathUtils.degToRad(-8);
-scene.add(trunk4);
+trunk.add(trunk4);
 
-// 나뭇잎
+tree.add(trunk);
+
+// 그룹화 - 나뭇잎
+const treeLeaf = new THREE.Group();
+
 const treeLeafMaterial = new THREE.MeshStandardMaterial({
   color: 0x84ad88,
   side: THREE.DoubleSide,
@@ -108,25 +115,31 @@ const treeLeafGeometry = new THREE.SphereGeometry(
 const treeLeaf1 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
 treeLeaf1.rotation.x = Math.PI / -2;
 treeLeaf1.position.set(0, 3.2, 2);
-scene.add(treeLeaf1);
+treeLeaf.add(treeLeaf1);
 
 const treeLeaf2 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
 treeLeaf2.rotation.x = Math.PI / -2;
 treeLeaf2.rotation.z = Math.PI / 2;
 treeLeaf2.position.set(2, 3.2, 0);
-scene.add(treeLeaf2);
+treeLeaf.add(treeLeaf2);
 
 const treeLeaf3 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
 treeLeaf3.rotation.x = Math.PI / -2;
 treeLeaf3.rotation.z = Math.PI;
 treeLeaf3.position.set(0, 3.2, -2);
-scene.add(treeLeaf3);
+treeLeaf.add(treeLeaf3);
 
 const treeLeaf4 = new THREE.Mesh(treeLeafGeometry, treeLeafMaterial);
 treeLeaf4.rotation.x = Math.PI / -2;
 treeLeaf4.rotation.z = Math.PI / -2;
 treeLeaf4.position.set(-2, 3.2, 0);
-scene.add(treeLeaf4);
+treeLeaf.add(treeLeaf4);
+
+tree.add(treeLeaf);
+treeLeaf.position.x = -0.4;
+treeLeaf.rotation.z = THREE.MathUtils.degToRad(-10);
+
+scene.add(tree);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
