f94e1835ad29b37130971c479bac802168958b7c
kijiwon <kiji1313@gmail.com>
2025-06-09 23:57:40 +0900

    여러 재질 적용

--- src/js/index.js
+++ src/js/index.js
@@ -13,56 +13,37 @@ const camera = new THREE.PerspectiveCamera(
   1000 // 최대거리
 );
 // 위치 설정
-camera.position.set(2, 2, 2);
+camera.position.set(3, 3, 3);
 // 바라볼 좌표값 설정 - camera는 z축에 평행한 방향을 비춤
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ canvas: $result, antialias: true });
 renderer.setSize($result.clientWidth, $result.clientHeight);
-renderer.shadowMap.enabled = true;
 
-// Texture
-const loader = new THREE.TextureLoader();
-const basecolor = loader.load("../../src/textures/bark/Bark_06_basecolor.jpg");
-const normal = loader.load("../../src/textures/bark/Bark_06_normal.jpg");
-const rough = loader.load("../../src/textures/bark/Bark_06_roughness.jpg");
-const height = loader.load("../../src/textures/bark/Bark_06_height.png");
-
-const geometry = new THREE.SphereGeometry(1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
-  // color: 0x2e6ff2,
-  // texture 연결
-  map: basecolor, // material 색상
-  normalMap: normal, // 표면에 빛을 왜곡시켜 입체감 표현
-  // normalScale: new THREE.Vector2(0, 0), // 빛의 왜곡 정도 조절. 기본값은 (1,1)
-  roughness: 0.4, // 거칠기를 줄여 입체감 확인
-
-  roughnessMap: rough, // 질감에 따른 빛의 굴곡 표현
-
-  displacementMap: height, // 텍스처의 명암에 따라 표면의 높낮이 조절(밝을수록 높고, 어두울수록 낮음)
-  displacementScale: 0.2, // 높낮이의 정도 설정
+  color: 0xffaaaa,
 });
-const cube = new THREE.Mesh(geometry, material);
 
-cube.castShadow = true;
-scene.add(cube);
+const materials = [
+  // 정육면제에 적용할 재질
+  new THREE.MeshStandardMaterial({ color: 0xff0000 }),
+  new THREE.MeshStandardMaterial({ color: 0xff8c00 }),
+  new THREE.MeshStandardMaterial({ color: 0xffee00 }),
+  new THREE.MeshStandardMaterial({ color: 0x4de94c }),
+  new THREE.MeshStandardMaterial({ color: 0x3783ff }),
+  new THREE.MeshStandardMaterial({ color: 0x4815aa }),
+];
 
-const geometry2 = new THREE.PlaneGeometry(10, 10);
-const material2 = new THREE.MeshStandardMaterial({
-  color: 0x81a8f7,
-  side: THREE.DoubleSide,
-});
-const plane = new THREE.Mesh(geometry2, material2);
-plane.rotation.x = Math.PI / -2;
-plane.position.y = -1;
-
-plane.receiveShadow = true;
-scene.add(plane);
+const mesh = new THREE.Mesh(geometry, materials);
+scene.add(mesh);
 
 const dl = new THREE.DirectionalLight(0xffffff, 1);
-dl.position.set(0, 2, 2);
-dl.castShadow = true;
+dl.position.set(2, 4, 3);
 scene.add(dl);
+dl.castShadow = true;
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
