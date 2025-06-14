33b83080a4b75aad1dd9e39a2097910029565b51
kijiwon <kiji1313@gmail.com>
2025-06-14 19:38:14 +0900

    fog

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+const fogColor = 0x004fff;
+const objColor = 0xffffff;
+const floorColor = 0x555555;
+
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x7ccad5);
+scene.background = new THREE.Color(floorColor);
+// fog
+scene.fog = new THREE.Fog(fogColor, 10, 50);
 
 const camera = new THREE.PerspectiveCamera(
   40,
@@ -10,68 +16,30 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(2, 2, 2);
+camera.position.set(0, 2, 3);
 camera.lookAt(0, 0, 0);
 
 const axesHelper = new THREE.AxesHelper(5);
 // scene.add(axesHelper);
 
+const geometry = new THREE.TorusGeometry(1, 0.5, 12, 80);
+const material = new THREE.MeshStandardMaterial({ color: objColor });
+const obj = new THREE.Mesh(geometry, material);
+obj.position.y = 0.8;
+scene.add(obj);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30, 1, 1);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: floorColor });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.y = -3;
+plane.rotation.x = -0.5 * Math.PI;
+scene.add(plane);
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
-// const geometry = new THREE.SphereGeometry(1);
-// const material = new THREE.MeshStandardMaterial({
-//   color: 0x2e6ff2,
-// });
-// const cube = new THREE.Mesh(geometry, material);
-// cube.castShadow = true;
-// scene.add(cube);
-
-// const geometry2 = new THREE.PlaneGeometry(10, 10);
-// const material2 = new THREE.MeshStandardMaterial({
-//   color: 0x81a8f7,
-//   side: THREE.DoubleSide,
-// });
-// const plane = new THREE.Mesh(geometry2, material2);
-// plane.rotation.x = Math.PI / -2;
-// plane.position.y = -1;
-// plane.receiveShadow = true;
-// scene.add(plane);
-
-const loader = new THREE.TextureLoader();
-const materials = [
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-1.jpg"),
-  }),
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-2.jpg"),
-  }),
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-3.jpg"),
-  }),
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-4.jpg"),
-  }),
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-5.jpg"),
-  }),
-  new THREE.MeshStandardMaterial({
-    map: loader.load("../src/textures/flowers/flower-6.jpg"),
-  }),
-];
-
-const geometry2 = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshStandardMaterial({ color: 0x2e6ff2 });
-const box = new THREE.Mesh(geometry2, materials);
-scene.add(box);
-
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-// directionalLight.position.set(-10, 10, 10);
-directionalLight.position.set(-1, 2, 2);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.width = 1024;
-directionalLight.shadow.mapSize.height = 1024;
-directionalLight.shadow.radius = 5;
+directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 
 const renderer = new THREE.WebGLRenderer();
