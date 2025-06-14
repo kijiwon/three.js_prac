ecd5d6133cf5763349f9f000348347b3b4d6aa72
kijiwon <kiji1313@gmail.com>
2025-06-14 18:11:56 +0900

    육면체 텍스쳐

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,78 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import printIsland from "../mesh/island.js";
-import printTangerine from "../mesh/tangerine.js";
-import printTree from "../mesh/tree.js";
-import printStone from "../mesh/stone.js";
-import printMountain from "../mesh/mountain.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x7ccad5);
 
 const camera = new THREE.PerspectiveCamera(
-  50,
+  40,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(0, 10, 20);
+camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
+
+// const geometry = new THREE.SphereGeometry(1);
+// const material = new THREE.MeshStandardMaterial({
+//   color: 0x2e6ff2,
+// });
+// const cube = new THREE.Mesh(geometry, material);
+// cube.castShadow = true;
+// scene.add(cube);
+
+// const geometry2 = new THREE.PlaneGeometry(10, 10);
+// const material2 = new THREE.MeshStandardMaterial({
+//   color: 0x81a8f7,
+//   side: THREE.DoubleSide,
+// });
+// const plane = new THREE.Mesh(geometry2, material2);
+// plane.rotation.x = Math.PI / -2;
+// plane.position.y = -1;
+// plane.receiveShadow = true;
+// scene.add(plane);
+
+const loader = new THREE.TextureLoader();
+const materials = [
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-1.jpg"),
+  }),
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-2.jpg"),
+  }),
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-3.jpg"),
+  }),
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-4.jpg"),
+  }),
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-5.jpg"),
+  }),
+  new THREE.MeshStandardMaterial({
+    map: loader.load("../src/textures/flowers/flower-6.jpg"),
+  }),
+];
+
+const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+const material2 = new THREE.MeshStandardMaterial({ color: 0x2e6ff2 });
+const box = new THREE.Mesh(geometry2, materials);
+scene.add(box);
+
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(-10, 10, 10);
+// directionalLight.position.set(-10, 10, 10);
+directionalLight.position.set(-1, 2, 2);
 directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.radius = 5;
 scene.add(directionalLight);
-
-const pl1 = new THREE.PointLight(0xff8c00, 1.5);
-pl1.position.set(4, 6, -3);
-scene.add(pl1);
-
-const pl2 = new THREE.PointLight(0xffe287, 2);
-pl2.position.set(-5.5, 3, 5);
-scene.add(pl2);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -39,39 +80,9 @@ renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
-const island = printIsland();
-scene.add(island);
-
-const tangerine = printTangerine();
-tangerine.position.set(-4, 2, 2.5);
-scene.add(tangerine);
-const miniTan = printTangerine();
-miniTan.position.set(-1, 1.5, 3);
-miniTan.scale.set(0.7, 0.7, 0.7);
-scene.add(miniTan);
-
-const tree = printTree();
-tree.position.set(7, 1, -1);
-tree.rotation.x = THREE.MathUtils.degToRad(10);
-tree.rotation.y = THREE.MathUtils.degToRad(15);
-scene.add(tree);
-const miniTree = printTree();
-miniTree.position.set(-7, 1, 0);
-miniTree.scale.set(0.8, 0.8, 0.8);
-scene.add(miniTree);
-
-const stone = printStone();
-stone.position.set(4, 1, 2.5);
-scene.add(stone);
-
-const mountain = printMountain();
-mountain.position.set(0, 3.5, -1.5);
-mountain.scale.set(2, 2);
-scene.add(mountain);
-
 // OrbitControls
 const control = new OrbitControls(camera, renderer.domElement);
-control.autoRotate = true;
+// control.autoRotate = true;
 control.autoRotateSpeed = -1;
 control.minDistance = 10;
 control.maxDistance = 30;
